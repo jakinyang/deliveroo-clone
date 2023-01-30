fac60f074c30c37a80383745e0813746e9c3f9b0
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-30 10:53:39 -0800

    --delete sanity

--- sanity/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'f48fo2rz',
-    dataset: 'production'
-  }
-})
--- sanity/schemas/index.js
+++
@@ -1,6 +0,0 @@
-import category from './category'
-import dish from './dish'
-import restaurant from './restaurant'
-import featured from './featured'
-
-export const schemaTypes = [restaurant, dish, category, featured]
