6369c7f63d9018242afdc730814e8f3dff4e6f96
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-29 16:01:30 -0800

    --add home screen with basic header structure

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,48 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from 'react-native-heroicons/outline'
 
 export function HomeScreen() {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white pt-4">
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+        <Image
+          source={{ uri: "https://links.papareact.com/wru" }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="#00CCBB" />
+          <TextInput
+            placeholder='Restaurants and cuisines'
+            keyboardType='default'
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+    </SafeAreaView>
   )
 }
