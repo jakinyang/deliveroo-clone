e29825a9cb76eb857ccdac6a526eca97d43c9a24
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-31 02:18:11 -0800

    --add gif

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,12 +1,16 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import * as Animatable from 'react-native-animatable';
 
 export function PreparingOrderScreen() {
   return (
     <SafeAreaView className="flex-1 bg-[#00CCBB] justify-center items-center">
-      <View>
-        <Text>PreparingOrderScreen</Text>
-      </View>
+      <Animatable.Image 
+        source={require('../assets/alegria-city.gif')}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-96 w-96"
+      />
     </SafeAreaView>
   )
 }
