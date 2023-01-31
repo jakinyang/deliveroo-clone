cf4ffa63a0759dc9568b89f7febb790fca13d353
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-31 02:57:53 -0800

    --fix stupid react currency depenency

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import { store } from './store';
 import { Provider } from 'react-redux';
 import { BasketScreen } from './screens/BasketScreen';
 import { PreparingOrderScreen } from './screens/PreparingOrderScreen';
+import { DeliveryScreen } from './screens/DeliveryScreen';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -31,6 +32,14 @@ export default function App() {
           options={{
             headerShown: false,
             presentation: 'fullScreenModal'
+          }}
+          />
+          <Stack.Screen 
+          name='Delivery' 
+          component={DeliveryScreen} 
+          options={{
+            presentation: 'fullScreenModal',
+            headerShown: false
           }}
           />
         </Stack.Navigator>
--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,10 +1,19 @@
 import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import * as Animatable from 'react-native-animatable';
 import * as Progress from 'react-native-progress';
+import { useNavigation } from '@react-navigation/native';
 
 
 export function PreparingOrderScreen() {
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate("Delivery")
+    }, 4000)
+  })
+
   return (
     <SafeAreaView className="flex-1 bg-[#FFFFFF] justify-center items-center">
       <Animatable.Image 
@@ -20,7 +29,7 @@ export function PreparingOrderScreen() {
       >
         Sending Order To Restaurant!
       </Animatable.Text>
-      <Progress.CircleSnail size={60} indeterminate={true} />
+      <Progress.CircleSnail size={60} indeterminate={true} color="#00CCBB" />
     </SafeAreaView>
   )
 }
