52f5c44d71c39e170dddb86b59c996734ec9cc07
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-01-30 17:14:21 -0800

    --add redux and dishrow

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,5 +1,8 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+import Currency from 'react-currency-formatter'
+import { urlFor } from '../sanity'
+import { MinusCircleIcon, PlusCircleIcon } from 'react-native-heroicons/solid'
 
 export function DishRow({
   id,
@@ -9,12 +12,55 @@ export function DishRow({
   image
 }) {
 
+  const [isPressed, setIsPressed] = useState(false)
   return (
-    <TouchableOpacity>
-      <View>
-        <Text className="text-lg mb-1">{name}</Text>
-        <Text className="text-gray-400">{description}</Text>
-      </View>
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity
+        className={`bg-white border p-4 border-gray-200 ${isPressed && "border-b-0"}`}
+        onPress={() => setIsPressed(!isPressed)}
+      >
+        <View
+          className="flex-row items-center"
+        >
+          <View className="flex-1 pr-2">
+            <Text className="text-lg mb-1">{name}</Text>
+            <Text className="text-gray-400">{description}</Text>
+            <Text className="text-gray-400 mt-2">
+              <Currency
+                quantity={price}
+                curency="USD"
+              />
+            </Text>
+          </View>
+          <View>
+            <Image
+              className="h-20 w-20 bg-gray-300 p-4"
+              source={{ uri: urlFor(image).url() }}
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+      {isPressed && (
+        <View className="bg-white px-4">
+          <View className="flex-row items-center space-x-2 pb-3">
+            <TouchableOpacity>
+              <MinusCircleIcon 
+                // color={items.length > 0 ? "#00CCBB" : "gray"}
+                color="#00CCBB"
+                size={40}
+              />
+            </TouchableOpacity>
+            <Text>0</Text>
+            <TouchableOpacity>
+              <PlusCircleIcon
+                // color={items.length > 0 ? "#00CCBB" : "gray"}
+                color="#00CCBB"
+                size={40}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+    </>
   )
 }
